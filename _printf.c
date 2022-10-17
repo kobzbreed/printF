e745d6973535f6b5f9f84816cc7ce4c3b2cdea76
kobzbreed <onajitekobim@gmail.com>
2022-10-17 14:26:51 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,112 +1,122 @@
 #include "main.h"
-/**
- * _printf - printf function
- *@format: const char pointer
- *Description: this functions implement some functions of printf
- Return: num of characteres printed
- */
-int _printf(const char *format, ...)
-{
-	const char *string;
-	int count = 0;
-	va_list arg;
-
-	if (!format)
-		return (-1);
-
-	va_start(arg, format);
-	string = format;
-
-	count = loop_format(arg, string);
-
-	va_end(arg);
-	return (count);
-}
-/**
- *loop_format - loop format
- *@arg: va_list arg
- *@string: pointer from format
- *Description: This function make loop tp string pointer
- Return: num of characteres printed
- */
-int loop_format(va_list arg, const char *string)
-{
-	int i = 0, flag = 0, count_fm = 0, count = 0, check_per = 0;
-	
-	while (i < _strlen((char *)string) && *string != '\0')
+	/**
+	 *_printf - printf
+	 *@format: const char pointer
+	 *Description: this functions implement some functions of printf
+	 *Return: num of characteres printed
+	 */
+	int _printf(const char *format, ...)
 	{
-		char aux = string[i];
+		const char *string;
+		int cont = 0;
+		va_list arg;
+	
 
-		if (aux == '%')
+		if (!format)
+			return (-1);
+	
+
+		va_start(arg, format);
+		string = format;
+	
+
+		cont = loop_format(arg, string);
+	
+
+		va_end(arg);
+		return (cont);
+	}
+	/**
+	 *loop_format - loop format
+	 *@arg: va_list arg
+	 *@string: pointer from format
+	 *Description: This function make loop tp string pointer
+	 *Return: num of characteres printed
+	 */
+	int loop_format(va_list arg, const char *string)
+	{
+		int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
+	
+
+		while (i < _strlen((char *)string) && *string != '\0')
 		{
-			i++, flag++;
-			aux = string[i];
-			if (aux == '\0' && _strlen((char *)string) == 1)
-				return (-1);
-			if (aux == '\0')
-				return (count);
+			char aux = string[i];
+	
+
 			if (aux == '%')
 			{
-				flag++;
-			} else
-			}
-				count_fm = function_manager(aux, arg);
-				if (count_fm >= 0 && count_fm != -1)
+				i++, flag++;
+				aux = string[i];
+				if (aux == '\0' && _strlen((char *)string) == 1)
+					return (-1);
+				if (aux == '\0')
+					return (cont);
+				if (aux == '%')
 				{
-					i++;
-					aux = string[i];
-					if (aux == '%')
-						flag--;
-					count = count + count_fm;
-				} else if (count_fm == -1 && aux != '\n')
+					flag++;
+				} else
 				{
-					count += _putchar('%');
+					cont_fm = function_manager(aux, arg);
+					if (cont_fm >= 0 && cont_fm != -1)
+					{
+						i++;
+						aux = string[i];
+						if (aux == '%')
+							flag--;
+						cont = cont + cont_fm;
+					} else if (cont_fm == -1 && aux != '\n')
+					{
+						cont += _putchar('%');
+					}
 				}
-
 			}
+			check_per = check_percent(&flag, aux);
+			cont += check_per;
+			if (check_per == 0 && aux != '\0' && aux != '%')
+				cont += _putchar(aux), i++;
+			check_per = 0;
 		}
-		check_per = check_percent(&flag, aux);
-		count += check_per;
-		if (check_per == 0 && aux != '\0' && aux != '%')
-			count += _putchar(aux), i++;
-		check_per = 0;
+		return (cont);
 	}
-	return (count);
-}
-/**
- * check_percent - call function manager
- *@flag: value by reference
- *@aux: character
- *Description: This function print % pear
- *Return: 1 if % is printed
- */
-int check_percent(int *flag, char aux)
-{
-	int tmp_flag;
-	int count = 0;
-
-	tmp_flag = *flag;
-	if (tmp_flag == 2 && aux == '%')
+	/**
+	 * check_percent - call function manager
+	 *@flag: value by reference
+	 *@aux: character
+	 *Description: This function print % pear
+	 *Return: 1 if % is printed
+	 */
+	int check_percent(int *flag, char aux)
 	{
-		_putchar('%');
-		tmp_flag = 0;
-		count = 1;
+		int tmp_flag;
+		int cont = 0;
+	
+
+		tmp_flag = *flag;
+		if (tmp_flag == 2 && aux == '%')
+		{
+			_putchar('%');
+			tmp_flag = 0;
+			cont = 1;
+		}
+		return (cont);
 	}
-	return (count);
-}
+	
 
-/**
- * call_function_manager - call function manager
- *@aux: character parameter
- *@arg: va_list arg
- *Description: This function call function manager
- *Return: num of characteres printed
- */
+	/**
+	 * call_function_manager - call function manager
+	 *@aux: character parameter
+	 *@arg: va_list arg
+	 *Description: This function call function manager
+	 *Return: num of characteres printed
+	 */
+	
 
-int call_function_manager(char aux, va_list arg)
-{
-	int count = 0;
+	int call_function_manager(char aux, va_list arg)
+	{
+		int cont = 0;
+	
 
-	count = function_manager(aux, arg);
-	return (count);
-}
+		cont = function_manager(aux, arg);
+		return (cont);
+	}
+
